8fc719405a138d04f295a647ff12ac5e31e7342b
transparenceco <bruno@transparence.co>
2022-01-07 08:01:36 -0500

    fixed scroll trigger

--- docs/c1/main.js
+++ docs/c1/main.js
@@ -1,27 +1,44 @@
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
+gsap.from(".logo", {duration:.5, y:-50, opacity:0,  ease:"power2"});
 
-gsap.from(".logo", {duration:.5, y:-50, opacity:0,  ease:"power2"
+gsap.from(".scrolldown", {delay:1, duration:2, y:-50, opacity:0,  ease:"elastic" });
+
+gsap.from(".highlight", {
+    scrollTrigger: {
+        trigger:".highlight",
+    },
+    x:-500,
+    duration:.5
 });
 
-gsap.from(".scrolldown", {delay:1, duration:2, y:-50, opacity:0,  ease:"elastic"
+gsap.from(".highlight2", {
+    scrollTrigger: {
+        trigger:".highlight2",
+    },
+    x:-500,
+    duration:.5
 });
 
 
-gsap.from(".highlight", {duration:.5, x:-300, ease:"linear",
- scrollTrigger:{
-     trigger:".highlight",
-     start:"top 75%",
-     end: "bottom 25%"
- }
-})
 
-gsap.from(".highlight2", {duration:.5, x:-300, ease:"linear",
- scrollTrigger:{
-     trigger:".highlight2",
-     start:"top 75%",
-     end: "bottom 25%"
- }
-})
+
+
+
+// gsap.from(".highlight", {duration:.5, x:-400, ease:"linear",
+//  scrollTrigger:{
+//      trigger:".highlight",
+//      start:"top 75%",
+//      end: "bottom 25%"
+//  }
+// })
+
+// gsap.from(".highlight2", {duration:.5, x:-400, ease:"linear",
+//  scrollTrigger:{
+//      trigger:".highlight2",
+//      start:"top 75%",
+//      end: "bottom 25%"
+//  }
+// })
 
 
